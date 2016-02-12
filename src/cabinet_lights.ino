1788c8cefde2191556afd3bfec9f3faa3ecddf58
Matthew Lewis <matt@mplewis.com>
2016-02-12 17:38:21 -0600

    logic: use shelves, debounce

--- src/cabinet_lights.ino
+++ src/cabinet_lights.ino
@@ -7,22 +7,23 @@ const char PIN_LEFT = 6;
 const char PIN_RIGHT = 7;
 
 const int NUM_LEDS = 18;
-const int LEDS_LEFT[] = {0, 1, 2, 9, 10, 11, 12, 13, 14};
-const int LEDS_RIGHT[] = {3, 4, 5, 6, 7, 8, 15, 16, 17};
+const int NUM_SHELVES = 3;
+const int LED_SHELVES_LEFT[3][3] = {{0, 1, 2}, {9, 10, 11}, {12, 13, 14}};
+const int LED_SHELVES_RIGHT[3][3] = {{3, 4, 5}, {6, 7, 8}, {15, 16, 17}};
 
 Bounce left = Bounce();
 Bounce right = Bounce();
 CRGB leds[NUM_LEDS];
 
 void setLeds(bool isLeft, CRGB color) {
-  const int *toSet;
-  if (isLeft) {
-    toSet = LEDS_LEFT;
-  } else {
-    toSet = LEDS_RIGHT;
-  }
-  for (int i = 0; i < NUM_LEDS / 2; i++) {
-    leds[toSet[i]] = color;
+  for (int shelf = 0; shelf < NUM_SHELVES; shelf++) {
+    for (int i = 0; i < NUM_LEDS / NUM_SHELVES / 2; i++) {
+      if (isLeft) {
+        leds[LED_SHELVES_LEFT[shelf][i]] = color;
+      } else {
+        leds[LED_SHELVES_RIGHT[shelf][i]] = color;
+      }
+    }
   }
 }
 
@@ -37,19 +38,18 @@ void setRight(CRGB color) {
 void setup() {
   pinMode(PIN_LEFT, INPUT_PULLUP);
   pinMode(PIN_RIGHT, INPUT_PULLUP);
+  left.attach(PIN_LEFT);
+  right.attach(PIN_RIGHT);
+
   FastLED.addLeds<WS2801, PIN_DATA, PIN_CLOCK>(leds, NUM_LEDS);
-  setLeft(CRGB::Red);
-  setRight(CRGB::Red);
-  FastLED.show();
-  delay(2000);
 }
 
 void loop() {
   left.update();
   right.update();
-  if (!digitalRead(PIN_LEFT)) {
+  if (left.fell()) {
     setLeft(CRGB::White);
-  } else {
+  } else if (left.rose()) {
     setLeft(CRGB::Black);
   }
   if (!digitalRead(PIN_RIGHT)) {
