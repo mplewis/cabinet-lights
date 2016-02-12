85b7bb44fee1fccdf22b9eda268989eb84196db0
Matthew Lewis <matt@mplewis.com>
2016-02-12 17:55:26 -0600

    feature: working independent fades

--- src/cabinet_lights.ino
+++ src/cabinet_lights.ino
@@ -15,24 +15,37 @@ Bounce left = Bounce();
 Bounce right = Bounce();
 CRGB leds[NUM_LEDS];
 
-void setLeds(bool isLeft, CRGB color) {
-  for (int shelf = 0; shelf < NUM_SHELVES; shelf++) {
-    for (int i = 0; i < NUM_LEDS / NUM_SHELVES / 2; i++) {
-      if (isLeft) {
-        leds[LED_SHELVES_LEFT[shelf][i]] = color;
-      } else {
-        leds[LED_SHELVES_RIGHT[shelf][i]] = color;
-      }
-    }
+unsigned char left_value = 255;
+unsigned char right_value = 255;
+char left_delta = 0;
+char right_delta = 0;
+
+void check_buttons() {
+  left.update();
+  right.update();
+  if (left.fell()) {
+    left_delta = 1;
+  } else if (left.rose()) {
+    left_delta = -1;
+  }
+  if (right.fell()) {
+    right_delta = 1;
+  } else if (right.rose()) {
+    right_delta = -1;
   }
 }
 
-void setLeft(CRGB color) {
-  setLeds(true, color);
-}
-
-void setRight(CRGB color) {
-  setLeds(false, color);
+void run_deltas() {
+  left_value = constrain(left_value + left_delta, 0, 255);
+  right_value = constrain(right_value + right_delta, 0, 255);
+  for (int shelf = 0; shelf < NUM_SHELVES; shelf++) {
+    for (int i = 0; i < NUM_LEDS / NUM_SHELVES / 2; i++) {
+      char led_num_left = LED_SHELVES_LEFT[shelf][i];
+      leds[led_num_left] = CHSV(0, 0, left_value);
+      char led_num_right = LED_SHELVES_RIGHT[shelf][i];
+      leds[led_num_right] = CHSV(0, 0, right_value);
+    }
+  }
 }
 
 void setup() {
@@ -45,17 +58,8 @@ void setup() {
 }
 
 void loop() {
-  left.update();
-  right.update();
-  if (left.fell()) {
-    setLeft(CRGB::White);
-  } else if (left.rose()) {
-    setLeft(CRGB::Black);
-  }
-  if (!digitalRead(PIN_RIGHT)) {
-    setRight(CRGB::White);
-  } else {
-    setRight(CRGB::Black);
-  }
+  check_buttons();
+  run_deltas();
   FastLED.show();
+  delay(3);
 }
